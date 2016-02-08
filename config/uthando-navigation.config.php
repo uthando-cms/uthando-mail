4724e54554de4719068c4002cb903eae30087c89
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-02-08 07:17:30 +0000

    added icon for navigation

--- config/uthando-navigation.config.php
+++ config/uthando-navigation.config.php
@@ -5,6 +5,12 @@ return [
         'admin' => [
             'mail-queue' => [
                 'label'     => 'Mail Queue',
+                'params' => [
+                    'icon' => 'fa-envelope',
+                ],
+                'params' => [
+                    'icon' => 'fa-envelope',
+                ],
                 'action'    => 'index',
                 'route'     => 'admin/mail-queue',
                 'resource'  => 'menu:admin'
